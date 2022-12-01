817ac95184942c10b76f4fc22ff96b9a86bb16f8
Alishan <mdsalishan999@gmail.com>
2022-12-01 10:40:07 +0530

    Add

---
+++ class3-printCommandFunctionalities-CipherSchools.py
@@ -0,0 +1,10 @@
+print("Alishan")
+print(1)
+print(1,2,3,"Alishan",sep=",")
+print("Hello")
+print("World")
+print("Hello",end=":")
+print("World")
+# Print can accept any number of args and seperated by space.
+# Print adds new line character in end of print call.
+# But can be modified using end.
