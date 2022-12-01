5d52dba81bac1a8cc17c10297d2c894da39ab6e7
Alishan <mdsalishan999@gmail.com>
2022-12-01 10:41:20 +0530

    Add

---
+++ class5-OperatorAndExpression-CipherSchools.py
@@ -0,0 +1,64 @@
+a=5+5
+b=10-5
+c=10/3
+print(a)
+print(b)
+print(c)
+d=10//3 #Floor division
+print(d)
+e=2**3 #Exponential_Operator
+print(e)
+f="Mohd"+" Alishan"
+print(f)
+#String_Concatination and Multiplication
+
+
+#Comparison_Operator
+g=1==2
+print(g)
+h="a"<"A"
+print(h)
+
+#Logical_Operator
+#not,and,or
+i=True and False
+print(i)
+j=True or False
+print(j)
+
+k=1 and 5
+print(k)
+l=isinstance(True,int)
+m=type(True)
+# 0 and empty string is falsy value and non-zero is truthy value.
+
+#Short_Circuiting
+# a or b=a (if a is truthy)
+# a or b=b (if a is falsy)
+# a and b=b (if a is truthy)
+# a and b=a (if a is faisy)
+
+# true or b =true
+# false or b= b
+# true and b=b
+# false and b=false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
