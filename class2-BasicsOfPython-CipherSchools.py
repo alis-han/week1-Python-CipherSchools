f20203bd120f6ba3171621294c78eb36f2a5b36b
Alishan <mdsalishan999@gmail.com>
2022-12-01 10:39:28 +0530

    Add

---
+++ class2-BasicsOfPython-CipherSchools.py
@@ -0,0 +1,23 @@
+print("Hello World")
+#---> means comment
+'''__Also Comment'''
+
+# Every line executed is either
+# 1.Statement
+# 2.Expression
+
+# Four Basics Data-Types:
+# 1.Integers
+# 2.Float
+# 3.String
+# 4.Complex
+
+# Every variable in python is Object.
+isinstance(print,object)
+
+a=256
+print(id(a))
+print(hex(id(a)))
+# Id gives Memory Location in int value only.
+# hex(id)--> in memory format.
+
