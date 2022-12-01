e051b5fb36cf9f709fc37cb213067e2a31eff748
Alishan <mdsalishan999@gmail.com>
2022-12-01 10:40:46 +0530

    Add

---
+++ class4-TakingUserInputAndTypeConversion-CipherSchools.py
@@ -0,0 +1,12 @@
+# delimiter is only \n character
+a=input()
+print(a)
+type(a)
+
+# input() function
+# typecasting:storing the original element and working in copied element.
+# typecoercion
+
+c=15
+str(c)
+isinstance(c,object)
